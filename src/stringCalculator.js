815071db140022c0728068bfca443357d10f7044
Druva Alur <druva.alur@gmail.com>
2024-10-15 23:35:20 +0530

    Implementing Custom Delimiter Logic

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,13 @@
 function add(numbers) {
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    const delimiter = parts[0].substring(2);
+    const nums = parts[1].split(delimiter).map(Number);
+    return nums.reduce((sum, num) => sum + num, 0);
+  }
+
   if (numbers === "") return 0;
+
   const nums = numbers.split(/[\n,]/).map(Number);
   return nums.reduce((sum, num) => sum + num, 0);
 }
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -15,3 +15,7 @@ test('returns 6 for "1,5"', () => {
 test('returns 6 for "1\\n2,3"', () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test('returns 3 for "//;\\n1;2"', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
