1b552a04fb7fd04dde3a76d97ede8f7ddffcc77a
Druva Alur <druva.alur@gmail.com>
2024-10-15 23:33:20 +0530

    Implement New Line Handling

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -11,3 +11,7 @@ test('returns 1 for "1"', () => {
 test('returns 6 for "1,5"', () => {
   expect(add("1,5")).toBe(6);
 });
+
+test('returns 6 for "1\\n2,3"', () => {
+  expect(add("1\n2,3")).toBe(6);
+});
