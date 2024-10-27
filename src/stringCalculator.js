8cc45426c7c12eae0509b11586755d6ade38ae27
Druva Alur <druva.alur@gmail.com>
2024-10-27 14:17:50 +0530

    refactoring code

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,45 +1,45 @@
 function escapeRegexCharacters(delimiter) {
   return delimiter.replace(/[-\/\\^$.*+?()[\]{}|]/g, "\\$&");
 }
-
-function add(numbers) {
+function checkForNegativeNums(nums) {
   const negatives = [];
-
-  if (numbers.startsWith("//")) {
-    const parts = numbers.split("\n");
-    const delimiterSection = parts[0].substring(2);
-
-    const delimiters = delimiterSection
-      .split("][")
-      .map((d) => d.replace(/[\[\]]/g, ""));
-    const escapedDelimiters = delimiters.map(escapeRegexCharacters);
-    const regexDelimiter = escapedDelimiters.join("|");
-
-    const nums = parts[1].split(new RegExp(regexDelimiter)).map(Number);
-
-    nums.forEach((num) => {
-      if (num < 0) negatives.push(num);
-      if (num > 1000) return;
-    });
-
-    if (negatives.length > 0)
-      throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
-
-    return nums.reduce((sum, num) => (num <= 1000 ? sum + num : sum), 0);
-  }
-
-  if (numbers === "") return 0;
-
-  const nums = numbers.split(/[\n,]/).map(Number);
-
   nums.forEach((num) => {
     if (num < 0) negatives.push(num);
   });
 
   if (negatives.length > 0)
     throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
+}
 
+function add(numbers) {
+  if (numbers === "") return 0;
+
+  let delimiterPattern = "\n|,";
+  let nums = numbers.split(new RegExp(delimiterPattern)).map(Number);
+
+  if (numbers.startsWith("//")) {
+    ({ delimiterPattern, nums } = getDelimiter(
+      numbers,
+      delimiterPattern,
+      nums
+    ));
+  }
+  checkForNegativeNums(nums);
   return nums.reduce((sum, num) => (num <= 1000 ? sum + num : sum), 0);
+}
+
+function getDelimiter(numbers, delimiterPattern, nums) {
+  const parts = numbers.split("\n");
+  const delimiterSection = parts[0].substring(2);
+
+  const delimiters = delimiterSection
+    .split("][")
+    .map((d) => d.replace(/[\[\]]/g, ""));
+  const escapedDelimiters = delimiters.map(escapeRegexCharacters);
+  delimiterPattern = escapedDelimiters.join("|");
+
+  nums = parts[1].split(new RegExp(delimiterPattern)).map(Number);
+  return { delimiterPattern, nums };
 }
 
 module.exports = { add };
